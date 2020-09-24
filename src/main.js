03032215cc4b2a58322bccb040b40b6aa68151e6
amberxyz <1985464597@qq.com>
2020-09-24 08:09:41 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+ /* jshint esversion: 6 */
+ import Vue from 'vue';
+ import App from './App.vue';
+ import router from './router';
+ import store from './store';
+ // 加载全局样式
+ import './styles/index.less';
 
-Vue.config.productionTip = false
+ Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+ new Vue({
+     router,
+     store,
+     render: h => h(App)
+ }).$mount('#app');
